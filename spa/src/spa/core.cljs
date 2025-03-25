d01be20b58265c42fc5c756267117bf4d6475434
am-katzest <am-katzest@proton.me>
2025-03-25 02:28:16 +0100

    dispatch events based on information in url on startup

--- spa/src/spa/core.cljs
+++ spa/src/spa/core.cljs
@@ -20,5 +20,16 @@
 
 (defn init []
   (re-frame/dispatch-sync [::events/initialize-db])
+  (let [stored (or (u/get-url-info) {})
+        quiz-id (:quiz-id stored)
+        id (some->  stored :user-id parse-long)
+        token (some-> id u/get-token)]
+    (cond
+      (and quiz-id id token)
+      (re-frame/dispatch-sync [::events/start-connection id quiz-id token])
+      quiz-id
+      (re-frame/dispatch-sync [::events/join-quiz quiz-id])
+      :else
+      (re-frame/dispatch-sync [::events/create-quiz])))
   (dev-setup)
   (mount-root))
