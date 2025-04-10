b5d68e9d7192460b6c5f021dc4fdf11526d19ceb
am-katzest <am-katzest@proton.me>
2025-04-10 23:51:26 +0200

    + event handler for adding questions

--- spa/src/spa/organizer/events.cljs
+++ spa/src/spa/organizer/events.cljs
@@ -1,2 +1,11 @@
 (ns spa.organizer.events
-  (:require [spa.events :as e :refer [reg-input-event]]))
+  (:require
+   [re-frame.core :as re-frame]
+   [spa.events :as e :refer [reg-input-event]]))
+
+(re-frame/reg-event-fx
+ ::add-question
+ (fn [{db :db} [_ type desc]]
+   {:db (assoc db :adding-question? false)
+    :dispatch [::e/process-input {:type :input/add-question
+                                  :desc (assoc desc :type type)}]}))
