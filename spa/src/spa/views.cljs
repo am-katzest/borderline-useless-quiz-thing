d14a95c86eccaca7f9ca1707d65f0a498b8dfb57
am-katzest <am-katzest@proton.me>
2025-04-10 18:15:38 +0200

    assorted changes to view

--- spa/src/spa/views.cljs
+++ spa/src/spa/views.cljs
@@ -1,22 +1,29 @@
 (ns spa.views
   (:require
    [re-frame.core :as re-frame]
-   [re-com.core :as re-com :refer [at]]
+   [re-com.core :as re-com]
    [spa.styles :as styles]
-   [spa.subs :as subs]
-   ))
+   [spa.view-utils :refer [sub evt]]
+   [spa.organizer.views :refer [organizer-panel]]
+   [spa.participant.views :refer [participant-panel]]
+   [spa.subs :as subs]))
 
-(defn title []
-  (let [name (re-frame/subscribe [::subs/name])]
-    [re-com/title
-     :src   (at)
-     :label (str "Hello from " @name)
-     :level :level1
-     :class (styles/level1)]))
+(defn page-of-text [text]
+  [re-com/title
+   :label text
+   :level :level1
+   :class (styles/level1)])
+
+
+
+(defn user-panel []
+  (condp = (sub ::subs/user-type)
+    :organizer [organizer-panel]
+    :participant [participant-panel]
+    "error :("))
 
 (defn main-panel []
-  [re-com/v-box
-   :src      (at)
-   :height   "100%"
-   :children [[title]
-              ]])
+  (if (sub ::subs/running?)
+    [user-panel]
+    [page-of-text "connecting"]
+    ))
