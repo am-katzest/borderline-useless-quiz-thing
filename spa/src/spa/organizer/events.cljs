bc401321b55c17f2cf1ee22b8f923175d1c72d1d
am-katzest <am-katzest@proton.me>
2025-04-11 17:00:13 +0200

    + event to handle organizer updating question

--- spa/src/spa/organizer/events.cljs
+++ spa/src/spa/organizer/events.cljs
@@ -1,7 +1,7 @@
 (ns spa.organizer.events
   (:require
    [re-frame.core :as re-frame]
-   [spa.events :as e :refer [reg-input-event+db]]))
+   [spa.events :as e :refer [reg-input-event reg-input-event+db]]))
 
 (reg-input-event+db
  ::add-question
@@ -16,6 +16,13 @@
    [(assoc db :current-question nil)
     {:type :input/remove-question
      :question-id id}]))
+
+(reg-input-event
+ ::question-updated
+ (fn [id body]
+   {:type :input/update-question
+    :question-id id
+    :question body}))
 
 (re-frame/reg-event-db
  ::show-add-question-ui
