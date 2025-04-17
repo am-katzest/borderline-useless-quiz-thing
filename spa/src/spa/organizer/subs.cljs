a32ca2bb667bb207889c19f159028ac8a50d80a2
am-katzest <am-katzest@proton.me>
2025-04-17 22:09:03 +0200

    + accessor for client points for organizer

--- spa/src/spa/organizer/subs.cljs
+++ spa/src/spa/organizer/subs.cljs
@@ -1,6 +1,7 @@
 (ns spa.organizer.subs
   (:require
    [re-frame.core :as re-frame]
+   [buqt.model.questions :as qs]
    [spa.subs :as base]))
 
 (re-frame/reg-sub
@@ -8,3 +9,11 @@
  :<- [::base/gui-state]
  (fn [state]
    (seq (:id->name state))))
+
+(re-frame/reg-sub
+ ::user-points
+ :<- [::base/gui-state]
+ (fn [state [_ id]]
+   (qs/tally-points-for-answers-organizer
+    (:questions state)
+    (get-in state [:participant->question->answer id]))))
