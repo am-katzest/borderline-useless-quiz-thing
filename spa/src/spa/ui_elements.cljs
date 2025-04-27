d0d416834fc00b69452d922db165e2c404d08d3f
am-katzest <am-katzest@proton.me>
2025-04-27 19:37:29 +0200

    add delay before displaying popover

--- spa/src/spa/ui_elements.cljs
+++ spa/src/spa/ui_elements.cljs
@@ -58,13 +58,30 @@
    :label (if val "T" "F")
    :on-click #(set (not val))])
 
-(defn hover-popover [anchor popover & [position]]
-  (let [showing? (r/atom false)]
+(defn hover-popover [anchor popover & [position time]]
+  (let [state (r/atom {:id 0
+                       :showing? false})
+        change (fn [state showing?] (-> state
+                                       (assoc :showing? showing?)
+                                       (update :id inc)))
+        show (fn [state] (change state true))
+        hide (fn [state] (change state false))
+
+        swap-if-id-is-unchanged
+        (fn [state id-at-the-time-of-hover]
+          (if (= id-at-the-time-of-hover (:id state))
+            (show state)
+            state))
+
+        make-swapper-that-checks-id
+        (fn []
+          (let [id-at-the-time-of-hover (:id @state)]
+            #(swap! state swap-if-id-is-unchanged id-at-the-time-of-hover)))]
     [(fn []
-       [:div {:on-mouse-out #(reset! showing? false)
-              :on-mouse-over #(reset! showing? true)}
+       [:div {:on-mouse-out #(swap! state hide)
+              :on-mouse-over #(js/setTimeout (make-swapper-that-checks-id) (or time 500))}
         [re-com/popover-anchor-wrapper
-         :showing? showing?
+         :showing? (:showing? @state)
          :anchor anchor
          :popover [re-com/popover-content-wrapper  :style {:color :black} :body  popover]
          :position (or position :below-center)]])]))
